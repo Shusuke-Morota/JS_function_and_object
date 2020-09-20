bc026572ccdd7df67781ee850da3ef9aa6721694
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-09-20 16:34:29 +0900

    [Add:] prototype_overview

---
+++ prototype/main.js
@@ -0,0 +1,22 @@
+
+// prototypeとは
+// オブジェクトに存在する特別なプロパティー。
+// コンストラクター関数と共に使用する。
+
+unction Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototypePerson.prototype.hello = function () {
+    console.log("hello " + this.name);
+}
+// メソッドとして追加したいプロパティをprototypeオブジェクトに格納しておくことで、
+// インスタンスごとにメソッドを出力することができる。
+
+const bob = new Person("Bob", 18);
+const tom = new Person("Tom", 33);
+const sun = new Person("Sun", 25);
+
+bob.hello();
+tom.hello();
